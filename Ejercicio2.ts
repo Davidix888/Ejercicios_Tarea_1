79cb3a3ade2a96e01f3fcd9a2f4b951234bfada8
Davidix888 <124806048+Davidix888@users.noreply.github.com>
2024-09-17 21:13:25 -0600

    Ejercicio 2

---
+++ Ejercicio2.ts
@@ -0,0 +1,11 @@
+//Luis David Ixquiac Sac
+//1521223
+
+let n1: number = 10;
+let n2: number = 5;
+
+console.log(n1 + n2 + " Suma");
+console.log(n1 - n2 + " Resta");
+console.log(n1 * n2 + " Multiplicacion");
+console.log(n1 / n2 + " Division");
+console.log(n1 % n2 + " Modulo");
