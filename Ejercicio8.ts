80c671929b55f5d2e2cf8e3fecf26f1386dff9ed
Davidix888 <124806048+Davidix888@users.noreply.github.com>
2024-09-17 21:16:36 -0600

    Ejercicio 8

---
+++ Ejercicio8.ts
@@ -0,0 +1,37 @@
+//Luis David Ixquiac
+//1521223
+export class Persona {
+    private nombre: string;
+    private edad: number;
+    private ciudad: string;
+
+    constructor(nombre: string, edad: number, ciudad: string) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.ciudad = ciudad;
+    }
+
+    public getNombre(): string {
+        return this.nombre;
+    }
+
+    public setNombre(nombre: string): void {
+        this.nombre = nombre;
+    }
+
+    public getEdad(): number {
+        return this.edad;
+    }
+
+    public setEdad(edad: number): void {
+        this.edad = edad;
+    }
+
+    public getCiudad(): string {
+        return this.ciudad;
+    }
+
+    public setCiudad(ciudad: string): void {
+        this.ciudad = ciudad;
+    }
+}
