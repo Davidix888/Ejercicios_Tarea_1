407316dfacd41ae541c217b8dff12ce3c1ef19d6
Davidix888 <124806048+Davidix888@users.noreply.github.com>
2024-09-17 21:15:53 -0600

    Ejercicio 6

---
+++ Ejercicio6.ts
@@ -0,0 +1,12 @@
+//Luis David Ixquiac
+//1521223
+
+let numeros: number[] = [1, 2, 3, 4, 5];
+let sumaTotal: number = 0;
+
+for (let numero of numeros) {
+    console.log("Número:", numero);
+    sumaTotal += numero;
+}
+
+console.log("Suma total:", sumaTotal);
