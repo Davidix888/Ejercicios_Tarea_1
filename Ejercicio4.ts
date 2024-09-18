2770e22e53dd6b6a4c62dbf1fb47f375f6e592ea
Davidix888 <124806048+Davidix888@users.noreply.github.com>
2024-09-17 21:14:43 -0600

    Ejercicio 4

---
+++ Ejercicio4.ts
@@ -0,0 +1,13 @@
+//Luis David Ixquiac Sac
+//1521223
+console.log("Numeros del 1 al 4 utlizando for:")
+for (let i = 1; i < 5; i++) {
+    console.log("Numero:", i);
+}
+console.log("")
+console.log("Numeros del 1 al 4 utilizando while:")
+let contador: number = 1;
+while (contador < 5) {
+    console.log("Numero:", contador);
+    contador++;
+}
