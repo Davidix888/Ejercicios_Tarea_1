4b62a1e2f224d7fc36f4d3e3bf59a3f6bc0b497b
Davidix888 <124806048+Davidix888@users.noreply.github.com>
2024-09-17 21:12:59 -0600

    Ejercicio 1

---
+++ Ejericicio1.ts
@@ -0,0 +1,14 @@
+//Luis David Ixquiac Sac
+//1521223
+
+let numero: number = 19;
+let texto1: string = "David";
+let texto2: string = " ";
+let texto3: string = "Ixquiac"
+let booleano: boolean = true;
+
+console.log("Tu edad es:", numero);
+console.log(texto1 + texto2 + texto3);
+console.log("Booleano:",booleano);
+
+
