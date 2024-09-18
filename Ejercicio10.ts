ee56623888ecd3fa5fe223d9e226a1593bcd6b20
Davidix888 <124806048+Davidix888@users.noreply.github.com>
2024-09-17 21:17:14 -0600

    Ejercicio 10

---
+++ Ejercicio10.ts
@@ -0,0 +1,9 @@
+//Luis David Ixquiac Sac
+//1521223
+
+let textoCompleto: string = "Hola como estas?";
+let palabras: string[] = textoCompleto.split(" ");
+
+for (let palabra of palabras) {
+    console.log("Palabra:", palabra);
+}
