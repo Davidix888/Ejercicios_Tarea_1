229c722002e30727352d1002ec50d495d8f2355f
Davidix888 <124806048+Davidix888@users.noreply.github.com>
2024-09-17 21:13:42 -0600

    Ejercicio 3

---
+++ Ejercicio3.ts
@@ -0,0 +1,12 @@
+//Luis David Ixquiac Sac
+//1521223
+
+let edad: number = 18;
+
+if (edad < 18) {
+    console.log("aun estas pequeñito");
+} else if (edad == 18) {
+    console.log("ya no estas tan pequeño");
+} else {
+    console.log("ya estas viejo");
+}
